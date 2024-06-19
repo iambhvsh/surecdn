49e44aae0cae11e3f1d14685175ee616847cc65c
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-19 20:49:25 +0530

    Add files via upload

---
+++ templates/js/smoothx.js
@@ -0,0 +1,151 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+  var dropdownMenu = document.getElementById('dropdownMenu');
+  if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+    dropdownMenu.style.display = 'block';
+  } else {
+    dropdownMenu.style.display = 'none';
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const searchInputDesktop = document.getElementById("searchInputDesktop");
+  const searchInputMobile = document.getElementById("searchInputMobile");
+  const searchResultsDesktop = document.getElementById("searchResultsDesktop");
+  const searchResultsMobile = document.getElementById("searchResultsMobile");
+
+  // Show search results
+  function showResults(results, resultsContainer) {
+    resultsContainer.innerHTML = "";
+    results.forEach(result => {
+      const anchor = document.createElement("a");
+      anchor.href = result.link;
+      anchor.textContent = result.name;
+      anchor.classList.add("block", "px-3", "py-2", "rounded-md", "text-base", "font-medium", "text-white", "hover:text-black");
+      resultsContainer.appendChild(anchor);
+    });
+    resultsContainer.classList.remove("hidden");
+    resultsContainer.classList.add("block");
+  }
+
+  // Hide search results
+  function hideResults(resultsContainer) {
+    resultsContainer.classList.remove("block");
+    resultsContainer.classList.add("hidden");
+  }
+
+  // Close search results when clicking outside
+  document.addEventListener("click", function(event) {
+    const isClickInsideDesktop = searchInputDesktop.contains(event.target) || searchResultsDesktop.contains(event.target);
+    const isClickInsideMobile = searchInputMobile.contains(event.target) || searchResultsMobile.contains(event.target);
+
+    if (!isClickInsideDesktop) {
+      hideResults(searchResultsDesktop);
+    }
+    if (!isClickInsideMobile) {
+      hideResults(searchResultsMobile);
+    }
+  });
+
+  // Fetch data from JSON file
+  async function fetchData() {
+    try {
+      const response = await fetch('data.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  // Filter results based on input
+  async function filterResults(query, resultsContainer) {
+    const data = await fetchData();
+    if (data) {
+      const filteredResults = data.filter(result => result.name.toLowerCase().includes(query.toLowerCase()));
+      showResults(filteredResults, resultsContainer);
+    } else {
+      hideResults(resultsContainer);
+    }
+  }
+
+  // Event listeners for input
+  searchInputDesktop.addEventListener("input", function() {
+    const query = this.value.trim();
+    if (query.length > 0) {
+      filterResults(query, searchResultsDesktop);
+    } else {
+      hideResults(searchResultsDesktop);
+    }
+  });
+
+  searchInputMobile.addEventListener("input", function() {
+    const query = this.value.trim();
+    if (query.length > 0) {
+      filterResults(query, searchResultsMobile);
+    } else {
+      hideResults(searchResultsMobile);
+    }
+  });
+
+  // Check saved theme
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    applyTheme(savedTheme);
+  }
+
+  const toggleButton = document.getElementById('theme-toggle');
+  toggleButton.addEventListener('click', function() {
+    const body = document.body;
+
+    if (body.classList.contains('bg-white')) {
+      applyTheme('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      applyTheme('light');
+      localStorage.setItem('theme', 'light');
+    }
+
+    // Reload page after 3 seconds
+    setTimeout(() => {
+      location.reload();
+    }, 1);
+  });
+
+  function applyTheme(theme) {
+    const body = document.body;
+    const main = document.getElementById('main');
+    const footer = document.getElementById('footer');
+    const cards = document.querySelectorAll('.bg-white');
+    const toggleButton = document.getElementById('theme-toggle');
+
+    if (theme === 'dark') {
+      body.classList.remove('bg-white', 'text-black');
+      body.classList.add('bg-black', 'text-white');
+      main.classList.remove('bg-white');
+      main.classList.add('bg-black');
+      footer.classList.add('bg-black');
+      footer.classList.add('border-zinc-700');
+      cards.forEach(card => {
+        card.classList.remove('bg-white', 'text-black');
+        card.classList.add('bg-zinc-900', 'text-white');
+      });
+      toggleButton.innerHTML = '<ion-icon name="sunny" class="text-2xl"></ion-icon>';
+      toggleButton.classList.remove('text-black');
+      toggleButton.classList.add('text-white');
+    } else {
+      body.classList.remove('bg-black', 'text-white');
+      body.classList.add('bg-white', 'text-black');
+      main.classList.remove('bg-black');
+      main.classList.add('bg-white');
+      footer.classList.add('bg-white');
+      footer.classList.remove('border-zinc-700');
+      cards.forEach(card => {
+        card.classList.remove('bg-zinc-900', 'text-white');
+        card.classList.add('bg-white', 'text-black');
+      });
+      toggleButton.innerHTML = '<ion-icon name="moon" class="text-2xl"></ion-icon>';
+      toggleButton.classList.remove('text-white');
+      toggleButton.classList.add('text-black');
+    }
+  }
+});
