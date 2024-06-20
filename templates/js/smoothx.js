80b1abc6b889e46b12d9d73671af28d3a2fe4ca1
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-20 10:08:08 +0530

    Update smoothx.js

--- templates/js/smoothx.js
+++ templates/js/smoothx.js
@@ -1,1 +1,167 @@
-"serviceWorker"in navigator&&window.addEventListener("load",()=>{navigator.serviceWorker.register("https://surecdn.vercel.app/templates/service-worker.js").then(e=>{console.log("ServiceWorker registration successful with scope: ",e.scope)}).catch(e=>{console.log("ServiceWorker registration failed: ",e)})}),document.getElementById("menuButton").addEventListener("click",function(){var e=document.getElementById("dropdownMenu");"none"===e.style.display||""===e.style.display?e.style.display="block":e.style.display="none"}),document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("searchInputDesktop"),t=document.getElementById("searchInputMobile"),s=document.getElementById("searchResultsDesktop"),a=document.getElementById("searchResultsMobile");function l(e){e.classList.remove("block"),e.classList.add("hidden")}async function i(){try{let e=await fetch("data.json"),t=await e.json();return t}catch(s){console.error("Error fetching data:",s)}}async function n(e,t){let s=await i();if(s){var a,n;let o=s.filter(t=>t.name.toLowerCase().includes(e.toLowerCase()));a=o,(n=t).innerHTML="",a.forEach(e=>{let t=document.createElement("a");t.href=e.link,t.textContent=e.name,t.classList.add("block","px-3","py-2","rounded-md","text-base","font-medium","text-white","hover:text-black"),n.appendChild(t)}),n.classList.remove("hidden"),n.classList.add("block")}else l(t)}document.addEventListener("click",function(i){let n=e.contains(i.target)||s.contains(i.target),o=t.contains(i.target)||a.contains(i.target);n||l(s),o||l(a)}),e.addEventListener("input",function(){let e=this.value.trim();e.length>0?n(e,s):l(s)}),t.addEventListener("input",function(){let e=this.value.trim();e.length>0?n(e,a):l(a)});let o=localStorage.getItem("theme");o&&r(o);let c=document.getElementById("theme-toggle");function r(e){let t=document.body,s=document.getElementById("main"),a=document.getElementById("footer"),l=document.querySelectorAll(".bg-white"),i=document.getElementById("theme-toggle");"dark"===e?(t.classList.remove("bg-white","text-black"),t.classList.add("bg-black","text-white"),s.classList.remove("bg-white"),s.classList.add("bg-black"),a.classList.add("bg-black"),a.classList.add("border-zinc-700"),l.forEach(e=>{e.classList.remove("bg-white","text-black"),e.classList.add("bg-zinc-900","text-white")}),i.innerHTML='<ion-icon name="sunny" class="text-2xl"></ion-icon>',i.classList.remove("text-black"),i.classList.add("text-white")):(t.classList.remove("bg-black","text-white"),t.classList.add("bg-white","text-black"),s.classList.remove("bg-black"),s.classList.add("bg-white"),a.classList.add("bg-white"),a.classList.remove("border-zinc-700"),l.forEach(e=>{e.classList.remove("bg-zinc-900","text-white"),e.classList.add("bg-white","text-black")}),i.innerHTML='<ion-icon name="moon" class="text-2xl"></ion-icon>',i.classList.remove("text-white"),i.classList.add("text-black"))}c.addEventListener("click",function(){let e=document.body;e.classList.contains("bg-white")?(r("dark"),localStorage.setItem("theme","dark")):(r("light"),localStorage.setItem("theme","light")),setTimeout(()=>{location.reload()},1)})});const clearButton=document.getElementById("clear-data");function showConsoleClearMessage(){console.log("%cClick here to clear localStorage data","color: blue; text-decoration: underline; cursor: pointer;")}function setupConsoleLink(){if(window.console&&console.log){var e;showConsoleClearMessage(),console.log=(e=console.log,function(t){"Click here to clear localStorage data"===t&&document.getElementById("clear-data").click(),e.apply(console,arguments)})}}clearButton.addEventListener("click",async function(){localStorage.clear(),console.log("Local storage cleared");let e=await caches.keys();for(let t of e)await caches.delete(t),console.log(`Cache ${t} cleared`);if("serviceWorker"in navigator){let s=await navigator.serviceWorker.getRegistrations();for(let a of s)a.unregister(),console.log("Service worker unregistered")}location.reload()}),setupConsoleLink();
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("https://surecdn.vercel.app/templates/service-worker.js")
+            .then(e => {
+                console.log("ServiceWorker registration successful with scope: ", e.scope);
+            })
+            .catch(e => {
+                console.log("ServiceWorker registration failed: ", e);
+            });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let e = document.getElementById("searchInputDesktop"),
+        t = document.getElementById("searchInputMobile"),
+        s = document.getElementById("searchResultsDesktop"),
+        a = document.getElementById("searchResultsMobile");
+
+    function l(e) {
+        e.classList.remove("block");
+        e.classList.add("hidden");
+    }
+
+    async function i() {
+        try {
+            let e = await fetch("data.json");
+            let t = await e.json();
+            return t;
+        } catch (s) {
+            console.error("Error fetching data:", s);
+        }
+    }
+
+    async function n(e, t) {
+        let s = await i();
+        if (s) {
+            let o = s.filter(t => t.name.toLowerCase().includes(e.toLowerCase()));
+            t.innerHTML = "";
+            o.forEach(e => {
+                let t = document.createElement("a");
+                t.href = e.link;
+                t.textContent = e.name;
+                t.classList.add("block", "px-3", "py-2", "rounded-md", "text-base", "font-medium", "text-white", "hover:text-black");
+                t.appendChild(t);
+            });
+            t.classList.remove("hidden");
+            t.classList.add("block");
+        } else {
+            l(t);
+        }
+    }
+
+    document.addEventListener("click", function(i) {
+        let n = e.contains(i.target) || s.contains(i.target),
+            o = t.contains(i.target) || a.contains(i.target);
+        if (!n) l(s);
+        if (!o) l(a);
+    });
+
+    e.addEventListener("input", function() {
+        let e = this.value.trim();
+        if (e.length > 0) n(e, s);
+        else l(s);
+    });
+
+    t.addEventListener("input", function() {
+        let e = this.value.trim();
+        if (e.length > 0) n(e, a);
+        else l(a);
+    });
+
+    let o = localStorage.getItem("theme");
+    if (o) r(o);
+
+    let c = document.getElementById("theme-toggle");
+
+    function r(e) {
+        let t = document.body,
+            s = document.getElementById("main"),
+            a = document.getElementById("footer"),
+            l = document.querySelectorAll(".bg-white"),
+            i = document.getElementById("theme-toggle");
+
+        if (e === "dark") {
+            t.classList.remove("bg-white", "text-black");
+            t.classList.add("bg-black", "text-white");
+            s.classList.remove("bg-white");
+            s.classList.add("bg-black");
+            a.classList.add("bg-black");
+            a.classList.add("border-zinc-700");
+            l.forEach(e => {
+                e.classList.remove("bg-white", "text-black");
+                e.classList.add("bg-zinc-900", "text-white");
+            });
+            i.innerHTML = '<ion-icon name="sunny" class="text-2xl"></ion-icon>';
+            i.classList.remove("text-black");
+            i.classList.add("text-white");
+        } else {
+            t.classList.remove("bg-black", "text-white");
+            t.classList.add("bg-white", "text-black");
+            s.classList.remove("bg-black");
+            s.classList.add("bg-white");
+            a.classList.add("bg-white");
+            a.classList.remove("border-zinc-700");
+            l.forEach(e => {
+                e.classList.remove("bg-zinc-900", "text-white");
+                e.classList.add("bg-white", "text-black");
+            });
+            i.innerHTML = '<ion-icon name="moon" class="text-2xl"></ion-icon>';
+            i.classList.remove("text-white");
+            i.classList.add("text-black");
+        }
+    }
+
+    c.addEventListener("click", function() {
+        let e = document.body;
+        if (e.classList.contains("bg-white")) {
+            r("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            r("light");
+            localStorage.setItem("theme", "light");
+        }
+        setTimeout(() => {
+            location.reload();
+        }, 1);
+    });
+});
+
+const menuButton = document.getElementById('menuButton');
+if (menuButton) {
+    menuButton.addEventListener('click', function() {
+        const dropdownMenu = document.getElementById('dropdownMenu');
+        if (dropdownMenu) {
+            if (dropdownMenu.classList.contains('opacity-0')) {
+                dropdownMenu.classList.remove('opacity-0', 'max-h-0');
+                dropdownMenu.classList.add('opacity-100', 'max-h-screen');
+            } else {
+                dropdownMenu.classList.remove('opacity-100', 'max-h-screen');
+                dropdownMenu.classList.add('opacity-0', 'max-h-0');
+            }
+        }
+    });
+}
+
+const clearButton = document.getElementById("clear-data");
+
+clearButton.addEventListener("click", async function() {
+    localStorage.clear();
+    console.log("Local storage cleared");
+
+    let e = await caches.keys();
+    for (let t of e) {
+        await caches.delete(t);
+        console.log(`Cache ${t} cleared`);
+    }
+
+    if ("serviceWorker" in navigator) {
+        let s = await navigator.serviceWorker.getRegistrations();
+        for (let a of s) {
+            a.unregister();
+            console.log("Service worker unregistered");
+        }
+    }
+
+    location.reload();
+});
